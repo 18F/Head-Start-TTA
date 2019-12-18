794e1398c1ab79d377a6b86eb05a331c4c8709b9
Ryan Ahearn <ryan.ahearn@gsa.gov>
2019-12-18 13:32:55 -0500

    Add circleci config

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "pages/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "pages/home.html.erb", type: :view do
+  it "displays the header" do
+    render
+    expect(rendered).to match "TTA Data Platform"
+  end
 end
