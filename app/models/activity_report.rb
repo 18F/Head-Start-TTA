6f3e64d25eff4c41d2700622b9417550b22e9dd5
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-01-17 11:53:01 -0500

    Link related reports together

--- app/models/activity_report.rb
+++ app/models/activity_report.rb
@@ -6,9 +6,16 @@ class ActivityReport < ApplicationRecord
   has_and_belongs_to_many :grants
   has_many :grantees, through: :grants
 
+  has_many :follow_up_reports, class_name: "ActivityReport", foreign_key: "previous_activity_report_id"
+  belongs_to :previous_activity_report, class_name: "ActivityReport", optional: true
+
   acts_as_ordered_taggable_on :materials, :topics
 
   auto_strip_attributes :activity_id
 
   update_index "activity_reports_index#activity_report", :self
+
+  def linked?
+    previous_activity_report.present? || follow_up_reports.any?
+  end
 end
--- app/models/grantee.rb
+++ app/models/grantee.rb
@@ -1,7 +1,7 @@
 class Grantee < ApplicationRecord
   validates_presence_of :name
 
-  has_many :grants
+  has_many :grants, dependent: :destroy
   has_many :activity_reports, through: :grants
 
   auto_strip_attributes :name
