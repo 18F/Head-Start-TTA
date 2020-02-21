1051e1deb758fbe9de5a58a0c3d4bd534e97d580
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-02-21 11:04:47 -0500

    Make api response examples consistent with domain model

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     role { "Grantee Specialist" }
     phone_number { FFaker::PhoneNumber.short_phone_number }
     email { FFaker::Internet.safe_email }
+
+    trait :grantee_employee do
+      role { "Head Start Director" }
+    end
   end
 end
