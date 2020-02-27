0d20127a0b7a0fbc2edb8b910ecf6a87e94ac811
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-02-27 16:06:20 -0500

    Reorgnize tta request form

--- app/javascript/tta_need/components/tta_need_form.jsx
+++ app/javascript/tta_need/components/tta_need_form.jsx
@@ -121,17 +121,17 @@ class TTANeedForm extends Component {
           <label className="usa-label" htmlFor="indicator">Indicator of Need</label>
           <input type="text" className="usa-input" id="indicator" value={indicator} readOnly />
           <p className="usa-hint">This Monitoring Report will be attached to request</p>
-          <label className="usa-label" htmlFor="additional-info">Additional Information</label>
-          <input type="text" className="usa-input" id="additional-info" value="attach files here" readOnly />
-          <label className="usa-label" htmlFor="narrative">Description of TTA Requested</label>
-          <textarea className="usa-textarea" id="narrative" value={narrative} name="narrative" onChange={this.inputChanged} />
-          <label className="usa-label" htmlFor="objectivs">Objectives</label>
+          <label className="usa-label" htmlFor="objectives">Outcomes for Grantee</label>
           <TaskList />
+          <label className="usa-label" htmlFor="narrative">Additional Information</label>
+          <textarea className="usa-textarea" id="narrative" value={narrative} name="narrative" onChange={this.inputChanged} />
+          <label className="usa-label" htmlFor="additional-info">Attachments</label>
+          <input type="text" className="usa-input" id="additional-info" value="attach files here" readOnly />
+          <div className="float-right">
+            <button type="button" className="usa-button usa-button--outline" onClick={() => { closeForm() }}>Cancel</button>
+            <button type="button" className="usa-button" onClick={() => { submitRequest() }}>Submit request</button>
+          </div>
         </form>
-        <div className="float-right">
-          <button type="button" className="usa-button usa-button--outline" onClick={() => { closeForm() }}>Cancel</button>
-          <button type="button" className="usa-button" onClick={() => { submitRequest() }}>Submit request</button>
-        </div>
       </div>
     )
   }
