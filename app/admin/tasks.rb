e086227b86fde3993a8100dd62c318f0ae980fe1
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-03-06 19:49:48 -0500

    Allow creating new person records when logging in

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Task do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,7 +13,6 @@ ActiveAdmin.register Task do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-
 
   form do |f|
     f.inputs "Details" do
@@ -34,5 +32,4 @@ ActiveAdmin.register Task do
     end
     f.actions
   end
-
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,12 @@ class SessionsController < ApplicationController
   def create
     return_path = session[:return_to] || root_path
     reset_session
-    session[:current_user_id] = params[:person_id]
+    if params[:person_id].present?
+      session[:current_user_id] = params[:person_id]
+    elsif params[:name].present?
+      new_person = Person.find_or_create_by(name: params[:name], role: "Program Specialist")
+      session[:current_user_id] = new_person.id
+    end
     redirect_to return_path
   end
 
