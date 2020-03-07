9c0a765c501ea609c70e18f01a97fb6eb623c988
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-03-06 19:40:26 -0500

    Allow fudging the creation date of a tta need request

--- app/admin/tta_needs.rb
+++ app/admin/tta_needs.rb
@@ -1,9 +1,10 @@
 ActiveAdmin.register TtaNeed do
-  permit_params :narrative
+  permit_params :narrative, :created_at
 
   form do |f|
     f.inputs "Markdown" do
       f.input :narrative
+      f.input :created_at
     end
     f.actions
   end
