dd39ce086cf796974ca007c6a408d78dc069382b
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-04-02 10:50:41 -0400

    Fix up tests

--- spec/views/grantees/show.html.erb_spec.rb
+++ spec/views/grantees/show.html.erb_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "grantees/show.html.erb", type: :view do
+  let(:grantee) { build_stubbed :grantee }
   before(:each) do
-    assign(:grantee, create(:grantee))
+    allow(grantee).to receive(:reviews).and_return []
+    assign(:grantee, grantee)
     assign(:activity_reports, Kaminari.paginate_array([]).page(1))
     assign(:monitoring_reports, [])
   end
