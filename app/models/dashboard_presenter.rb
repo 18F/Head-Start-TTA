691c77af52970473e8397867638a8c7ec9096b19
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-04-20 17:01:23 -0400

    Add timeline of deployment requests

--- app/models/dashboard_presenter.rb
+++ app/models/dashboard_presenter.rb
@@ -1,4 +1,15 @@
 class DashboardPresenter < SimpleDelegator
+  def deployments_by_month
+    month_counter = Hash.new { |h,k| h[k] = 0 }
+    request_sheet.each do |row|
+      requested = Time.parse(row.created).in_time_zone("America/New_York")
+      if requested > 1.year.ago.beginning_of_month
+        month_counter[requested.strftime("%b %Y")] += 1
+      end
+    end
+    month_counter
+  end
+
   def assigned_specialists_counts
     specialists = Person.tta_specialists
     assigned, available = specialists.partition { |p| assignment_sheet.has_upcoming_activity?(p.name) }
