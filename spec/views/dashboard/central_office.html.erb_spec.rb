eff62459bab6bbc6df1e505eeb24d63a4aed2650
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-05-12 17:09:40 -0400

    Use mocks for dashboard view test

--- spec/views/dashboard/central_office.html.erb_spec.rb
+++ spec/views/dashboard/central_office.html.erb_spec.rb
@@ -1,8 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "dashboard/central_office.html.erb", type: :view do
+  let(:dashboard) do
+    double(
+      deployments_by_month: [],
+      request_sheet: [],
+      plan_sheet: double(each_upcoming_activity: []),
+      assigned_specialists_counts: 0,
+      report_sheet: double(most_recent_activities: [])
+    )
+  end
   before(:each) do
-    assign(:dashboard, DashboardPresenter.new({}))
+    assign(:dashboard, dashboard)
   end
 
   it "renders the view" do
