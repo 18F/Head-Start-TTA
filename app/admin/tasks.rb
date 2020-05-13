23adc8d97052c9e832186d9e2c87cb6ed06f996b
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-05-13 10:06:38 -0400

    Enable markdown for goals & objectives titles

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -6,18 +6,41 @@ ActiveAdmin.register Task do
     redirect_to collection_path, alert: "The tasks have been reset."
   end
 
-  permit_params :status, :title, :notes, :parent_type, :parent_id, :created_by_id, :assigned_to_id, :completed_by_id, :due_date, :completed_at, :created_at
+  permit_params :status, :title, :notes, :parent_type, :parent_id, :created_by_id, :assigned_to_id, :completed_by_id, :due_date, :completed_at, :created_at, links: []
+
+  controller do
+    def create
+      parse_links
+      super
+    end
+
+    def update
+      parse_links
+      super
+    end
+
+    private def parse_links
+      params["task"]["links"] = begin
+                                  JSON.parse(params["task"]["links"])
+                                rescue
+                                  []
+                                end
+    end
+  end
 
   form do |f|
     f.inputs "Details" do
       f.input :status, as: :select, collection: %w[todo complete dropped]
-      f.input :title
-      f.input :notes
       f.input :parent_type
       f.input :parent_id
       f.input :due_date
       f.input :completed_at
       f.input :created_at
+      f.input :links
+    end
+    f.inputs "Markdown" do
+      f.input :title, as: :text
+      f.input :notes
     end
     f.inputs "People" do
       f.input :created_by
