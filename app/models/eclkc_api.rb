59b6cd82a8a25ccdf63ab46741728505f8fbce34
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-06-09 08:46:39 -0400

    Call into ECLKC API for centers and programs

--- app/models/eclkc_api.rb
+++ app/models/eclkc_api.rb
@@ -3,15 +3,30 @@ class EclkcApi
   CENTER_API_ROUTE = "#{BASE_API_ROUTE}/center".freeze
   PROGRAM_API_ROUTE = "#{BASE_API_ROUTE}/program".freeze
 
-  attr_reader :client
+  attr_reader :client, :response, :response_json
   def initialize
     @client = Faraday.new(
       url: "https://eclkc.ohs.acf.hhs.gov",
+      params: {apikey: Rails.application.credentials.eclkc_api_token},
       headers: {"Content-Type" => "application/json"}
     )
   end
 
-  def centers
-    []
+  def centers(query_params)
+    @response = client.get(CENTER_API_ROUTE) do |req|
+      req.params = req.params.merge query_params
+    end
+    @response_json = JSON.parse(response.body).with_indifferent_access
+  end
+
+  def programs(query_params)
+    @response = client.get(PROGRAM_API_ROUTE) do |req|
+      req.params = req.params.merge query_params
+    end
+    @response_json = JSON.parse(response.body).with_indifferent_access
+  end
+
+  def error_response?
+    response_json.has_key?(:error) || response_json.has_key?(:errors)
   end
 end
--- spec/models/eclkc_api_spec.rb
+++ spec/models/eclkc_api_spec.rb
@@ -3,7 +3,17 @@ require "rails_helper"
 RSpec.describe EclkcApi, type: :model do
   describe "#centers" do
     it "returns a list of centers" do
-      expect(subject.centers.class.name).to eq "Array"
+      centers_response = subject.centers state: "MD"
+      expect(centers_response.keys).to eq %w[results documents]
+      expect(centers_response[:results]).to eq 231
+    end
+  end
+
+  describe "#programs" do
+    it "returns a list of programs" do
+      programs_response = subject.programs state: "MD"
+      expect(programs_response.keys).to eq %w[results documents]
+      expect(programs_response[:results]).to eq 60
     end
   end
 end
