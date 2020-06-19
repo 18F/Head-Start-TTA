fc7f032b1177e4a8b74a65994c0491c21f56d8ac
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-06-19 13:45:23 -0400

    Update activity report full text search

--- app/models/activity_report.rb
+++ app/models/activity_report.rb
@@ -9,6 +9,7 @@ class ActivityReport < ApplicationRecord
 
   belongs_to :tta_need, optional: true
   has_many :topics, through: :tta_need
+  has_many :tasks, through: :tta_need
 
   belongs_to :activity_plan, optional: true
 
@@ -16,5 +17,17 @@ class ActivityReport < ApplicationRecord
 
   def region
     grants.map(&:region).uniq.compact.join(", ")
+  end
+
+  def linked?
+    !tta_need.nil?
+  end
+
+  def tta_need_reports
+    if linked?
+      tta_need.activity_reports
+    else
+      []
+    end
   end
 end
