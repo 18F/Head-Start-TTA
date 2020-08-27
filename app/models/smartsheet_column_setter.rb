ba4d4455c7951551ebf19e217a817a5dd457050d
Ryan Ahearn <ryan.ahearn@gsa.gov>
2020-08-27 13:15:14 -0400

    Update specialist options from staffing roster

--- app/models/smartsheet_column_setter.rb
+++ app/models/smartsheet_column_setter.rb
@@ -8,12 +8,12 @@ class SmartsheetColumnSetter
 
   def update_grantees
     each_region do |region|
-      grantee_name_column_title = column_title_for(region, :ar_details_grantee_column)
-      body = {type: "MULTI_PICKLIST", options: grantee_name_options(region), validation: true}
       sheet_id = SHEET_ID_CONFIG[:regions][region][:ar_combined_sheet]
       if sheet_id.present?
         sheet = client.sheets.get(sheet_id: sheet_id)
+        grantee_name_column_title = column_title_for(region, :ar_grantee_column)
         grantee_name_column_id = sheet[:columns].find { |c| c[:title] == grantee_name_column_title }.try(:[], :id)
+        body = {type: "MULTI_PICKLIST", options: grantee_name_options(region), validation: true}
         client.sheets.columns.update(sheet_id: sheet_id, column_id: grantee_name_column_id, body: body) unless grantee_name_column_id.nil?
       end
     end
@@ -21,13 +21,26 @@ class SmartsheetColumnSetter
 
   def update_topics
     each_region do |region|
-      topics_column_title = column_title_for(region, :ar_objectives_topics_column)
-      body = {type: "MULTI_PICKLIST", options: topics_options, validation: true}
       sheet_id = SHEET_ID_CONFIG[:regions][region][:ar_combined_sheet]
       if sheet_id.present?
         sheet = client.sheets.get(sheet_id: sheet_id)
+        topics_column_title = column_title_for(region, :ar_topics_column)
         topic_column_id = sheet[:columns].find { |c| c[:title] == topics_column_title }.try(:[], :id)
+        body = {type: "MULTI_PICKLIST", options: topics_options, validation: true}
         client.sheets.columns.update(sheet_id: sheet_id, column_id: topic_column_id, body: body) unless topic_column_id.nil?
+      end
+    end
+  end
+
+  def update_specialists
+    each_region do |region|
+      sheet_id = SHEET_ID_CONFIG[:regions][region][:ar_combined_sheet]
+      if sheet_id.present?
+        sheet = client.sheets.get(sheet_id: sheet_id)
+        specialists_column_title = column_title_for(region, :ar_specialists_column)
+        specialists_column_id = sheet[:columns].find { |c| c[:title] == specialists_column_title }.try(:[], :id)
+        body = {type: "MULTI_PICKLIST", options: specialists_options(region), validation: true}
+        client.sheets.columns.update(sheet_id: sheet_id, column_id: specialists_column_id, body: body) unless specialists_column_id.nil?
       end
     end
   end
@@ -68,5 +81,20 @@ class SmartsheetColumnSetter
 
   def grantee_names_sheet_id(region)
     SHEET_ID_CONFIG[:regions][region][:grantee_names_sheet]
+  end
+
+  def specialists_options(region)
+    return [] if specalists_sheet_id(region).blank?
+    specialists_sheet(region)[:rows].map { |row| row[:cells] }.map { |cells|
+      "#{cells[1][:display_value]} <#{cells[0][:display_value]}>"
+    }
+  end
+
+  def specialists_sheet(region)
+    client.sheets.get(sheet_id: specalists_sheet_id(region))
+  end
+
+  def specialists_sheet_id(region)
+    SHEET_ID_CONFIG[:regions][region][:specialists_sheet]
   end
 end
