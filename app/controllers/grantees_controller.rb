209644d72c4f32a1bdec0e12177489ee2b8dfa4b
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-02-23 13:42:26 -0500

    Refactor search to remove elasticsearch

--- app/controllers/grantees_controller.rb
+++ app/controllers/grantees_controller.rb
@@ -12,10 +12,8 @@ class GranteesController < ApplicationController
       @page_title = @grantee.name
       @page_has_custom_main = true
       @query = params[:q]
-      @topic = params[:topic]
-      @date = params[:date]
       @monitoring_reports = @grantee.monitoring_reports.order(:report_date)
-      @activity_reports = find_activity_reports
+      @activity_reports = find_activity_reports.distinct.page(page).per(per_page).order(start_date: :desc)
     end
     respond_to do |format|
       format.html
@@ -26,19 +24,9 @@ class GranteesController < ApplicationController
   private
 
   def find_activity_reports
-    if @query.present? || @topic.present? || @date.present?
-      search = ActivityReportsIndex.for_grantee(@grantee.id)
-      search = search.filter_topic(@topic) if @topic.present?
-      search = search.filter_start_date(@date) if @date.present?
-      search = if @query.present?
-        search.search(@query)
-      else
-        search.order(start_date: {order: :desc})
-      end
-      search.page(page).per(per_page).objects
-    else
-      @grantee.activity_reports.distinct.page(page).per(per_page).order(start_date: :desc)
-    end
+    search = @grantee.activity_reports
+    search = search.where("contact_method ilike '?%'", @query) if @query.present?
+    search
   end
 
   def page
